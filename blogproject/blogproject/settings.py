6ae0ee0a078e4fd18e273521fe0e1f803a7ffc99
WenJian <w1248153523@163.com>
2017-09-22 16:46:57 +0800

    紧急修复版 1.0 .Ubuntu部署版本

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@a8c7)0i^l)bzc*8qv3^7b&pql03avj+7w+#1_exw$*^&(*hj#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.zxcvb492.xyz']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
